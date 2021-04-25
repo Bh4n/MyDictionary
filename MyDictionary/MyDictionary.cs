c69b027909d7e8eade4177439d9ae4a637fac4a7
Bh4n <Bh4n@users.noreply.github.com>
2021-04-26 02:49:05 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<Tkey, Tvalue>
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public MyDictionary()
+        {
+            keys = new Tkey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey tkey, Tvalue tvalue)
+        {
+            Tkey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = tkey;
+            values[values.Length - 1] = tvalue;
+        }
+
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine("Anahtar: " + keys[i] + " Değer: " + values[i]);
+            }
+        }
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, String> cities = new MyDictionary<int, string>();
+            cities.Add(1, "Adana");
+            cities.Add(2, "Adıyaman");
+
+            Console.WriteLine("Length: " + cities.Length);
+
+            cities.Add(3, "Afyon");
+            cities.Add(4, "Ağrı");
+            cities.Add(5, "Amasya");
+            cities.Add(6, "Ankara");
+
+            Console.WriteLine("Length: " + cities.Length);
+
+            cities.List();
+        }
+    }
+}
